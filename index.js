f4a2082e12dab08228d50c831c964fea75618d72
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-18 20:23:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function  thetruth(10,15) {
+  if (10 === 15) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function sayHey(){
+  return ("Hey!");
+}
+function sayHeyFriend(name){
+  return ("Hey," + name );
+}
+
+function favoriteIceCream (){
+return ("I love mint chocolate chip" )
+}
+function shouting(){
+  return("I LOVE JAVASCRIPT SOOOOOO MUCH");
+}
+
+function roundDown(Number){
+  return(Math.floor(Number))
+}
